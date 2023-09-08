d8089f1db776c2d6cc84f8377dc273b8af2f2377
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-09-08 10:56:29 -0500

    Add admin button to footer

--- src/components/NetlifyIdentity.svelte
+++ src/components/NetlifyIdentity.svelte
@@ -1,14 +1,24 @@
 <script>
   import { onMount } from "svelte";
+  let isLoggedIn;
   onMount(() => {
     if (window.netlifyIdentity) {
       window.netlifyIdentity.on("init", (user) => {
         if (!user) {
+          isLoggedIn = false;
           window.netlifyIdentity.on("login", () => {
             document.location.href = "/admin/";
           });
+        } else {
+          isLoggedIn = true;
         }
       });
     }
   });
 </script>
+
+{#if isLoggedIn}
+  <a href="https://toupin.netlify.app/admin">Backend</a>
+{:else}
+  <a href="https://toupin.netlify.app/admin">Login</a>
+{/if}
